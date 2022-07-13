f282eb39e996f51f428c1b80a912c40120ae4588
1804011 <sharifsharifulislam061@gmail.com>
2022-07-13 23:08:40 +0600

    contest rating chart added

---
+++ src/Components/ContestChart.js
@@ -0,0 +1,78 @@
+import React, { useContext } from "react";
+import { useQuery } from "react-query";
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Area,
+  Tooltip,
+} from "recharts";
+import { userContext } from "./Home";
+
+const ContestChart = () => {
+  const { user } = useContext(userContext);
+  let { data, isLoading } = useQuery("ratedContest", () =>
+    fetch(
+      `https://lit-meadow-72602.herokuapp.com/profile/contests/${user?.displayName}`
+    ).then((res) => res.json())
+  );
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  for (let i = 0; i < data?.length; i++) {
+    let sz = data[i]?.newRating?.length;
+    for (let j = 0; j < sz; j++) {
+      if (data[i]?.newRating[j]?.username == user?.displayName) {
+        const { newRating, previousRating, position } = data[i]?.newRating[j];
+        data[i].newRating = newRating;
+        data[i].previousRating = previousRating;
+        data[i].ratingChange = newRating - previousRating;
+        data[i].rank = position;
+      }
+      if (data[i]?.standing[j]?.handle == user?.displayName) {
+        data[i].solved = data[i].standing[j].ok;
+      }
+    }
+  }
+  let sz = data.length;
+  let arr = [];
+  for (let i = sz - 1; i >= 0; i--) {
+    arr.push({
+      name: data[i]?.id,
+      rating: data[i].newRating,
+    });
+  }
+  data = arr;
+  return (
+    <div>
+      <AreaChart
+        width={750}
+        height={250}
+        data={data}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+
+        <Area
+          type="monotone"
+          dataKey="rating"
+          stroke="#82ca9d"
+          fillOpacity={1}
+          fill="url(#colorPv)"
+        />
+      </AreaChart>
+    </div>
+  );
+};
+
+export default ContestChart;
--- src/Components/ProfileInfo.js
+++ src/Components/ProfileInfo.js
@@ -3,6 +3,7 @@ import { userContext } from "./Home";
 import ratings from "../Images/ratings.png";
 import mail from "../Images/mail.png";
 import { profileContext } from "./ProfileDrawer";
+import ContestChart from "./ContestChart";
 const ProfileInfo = () => {
   const { user } = useContext(userContext);
   const { profile } = useContext(profileContext);
@@ -14,31 +15,36 @@ const ProfileInfo = () => {
       .then((data) => setRating(data?.rating));
   }, [user]);
   return (
-    <div className="border my-1 px-4 py-6" style={{ borderRadius: "6px" }}>
-      <h1 className="font-bold text-[rgba(0,0,0,0.5)]">Newbie</h1>
-      <h1 className="font-bold text-[rgba(0,0,0,0.5)] text-[24px]">
-        {user?.displayName}
-      </h1>
-      <p className="text-[14px]">
-        <span>{fullName}</span>,
-        <span className="text-[blue] underline pl-[2px]">{city}</span>,
-        <span className="text-[blue] underline pl-[2px]">{country}</span>
-      </p>
-      <p className="text-[14px]">
-        <span>From</span>,
-        <span className="text-[blue] underline pl-[2px]">{institute}</span>
-      </p>
-      <div className="flex my-3">
-        <img src={ratings} alt="rating"></img>
+    <div>
+      <div className="border my-1 px-4 py-6" style={{ borderRadius: "6px" }}>
+        <h1 className="font-bold text-[rgba(0,0,0,0.5)]">Newbie</h1>
+        <h1 className="font-bold text-[rgba(0,0,0,0.5)] text-[24px]">
+          {user?.displayName}
+        </h1>
+        <p className="text-[14px]">
+          <span>{fullName}</span>,
+          <span className="text-[blue] underline pl-[2px]">{city}</span>,
+          <span className="text-[blue] underline pl-[2px]">{country}</span>
+        </p>
+        <p className="text-[14px]">
+          <span>From</span>,
+          <span className="text-[blue] underline pl-[2px]">{institute}</span>
+        </p>
+        <div className="flex my-3">
+          <img src={ratings} alt="rating"></img>
 
-        <span className="pl-2 font-semibold text-[18px]">
-          Contest Rating: {rating}
-        </span>
+          <span className="pl-2 font-semibold text-[18px]">
+            Contest Rating: {rating}
+          </span>
+        </div>
+        <div className="flex">
+          <img src={mail} alt="mail"></img>
+
+          <span className="pl-2 font-semibold text-[18px]">{user?.email}</span>
+        </div>
       </div>
-      <div className="flex">
-        <img src={mail} alt="mail"></img>
-
-        <span className="pl-2 font-semibold text-[18px]">{user?.email}</span>
+      <div className="my-6 border p-5 py-8" style={{ borderRadius: "6px" }}>
+        <ContestChart />
       </div>
     </div>
   );
